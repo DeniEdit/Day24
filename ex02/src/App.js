cb392137f79de765fa9f0cbfa7f65319d14d9b2b
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-03-31 20:36:17 +0200

    Exercise 02

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-
-class App extends React.Component {
-
-  getMyStorage = () => {
-    document.cookie = "Year=2021";
-    localStorage.getItem('Paragon', 'yes, but Arena first');
-    sessionStorage.getItem('frontend', 'React');
-  }
-
-    render() {
-        return (
-            <div className="App">
-               <button type="button" onClick={this.getMyStorage}> My storage </button>
-            </div>
-        );
-    }
+import React, { Component } from "react";
+class App extends Component {
+	setMyStorage = () => {
+		document.cookie = "Year=2021";
+		localStorage.setItem("Paragon", "yes, but Arena first");
+		sessionStorage.setItem("frontend", "React");
+	};
+	getMyStorage = () => {
+		let myCookieData = document.cookie.match(new RegExp("(^| )Year=([^;]+)"))[2];
+		let myLocalStorageData = localStorage.getItem("Paragon");
+		let mySessionStorageData = sessionStorage.frontend;
+	};
+	render() {
+		return (
+			<div className="App">
+				<button onClick={this.setMyStorage}>Set My Storage</button>
+				<button onClick={this.getMyStorage}>Get My Storage</button>
+			</div>
+		);
+	}
 }
 export default App;
