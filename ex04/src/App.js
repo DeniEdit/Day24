2b501216bf49804528f646120a4275a1443e74f0
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-04-01 08:36:26 +0200

    Exercise 04

--- ex04/src/App.js
+++ ex04/src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-
-class App extends React.Component {
-
-  getLocalStorage = () => {
-    localStorage.getItem('Arena');
-  }
-
-    render() {
-        return (
-            <div className="App">
-               <button type="button" onClick={this.getLocalStorage}> Local storage </button>
-            </div>
-        );
-    }
-}
-
-export default App;
+ import React, { Component } from "react";
+ 
+ class App extends Component {
+   setLocalStorage = () => {
+     localStorage.setItem("Arena", "Selection Month");
+   };
+ 
+   getLocalStorage = () => {
+     let myLocalStorageData = localStorage.getItem("Arena");
+     console.log(myLocalStorageData);
+   };
+ 
+   render() {
+     return (
+       <div className="App">
+         <button onClick={this.setLocalStorage}>Set Local Storage</button>
+         <button onClick={this.getLocalStorage}>Get Local Storage</button>
+       </div>
+     );
+   }
+ }
+ 
+ export default App;
